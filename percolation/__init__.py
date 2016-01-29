f7502eda7308696c16dd1cadfeed0b7e609d92a3
Renato Fabbri <renato.fabbri@gmail.com>
2016-01-29 09:20:56 -0200

    adicionado query na raiz do percolation

--- percolation/__init__.py
+++ percolation/__init__.py
@@ -2,4 +2,4 @@ from .bootstrap import start, close, check, QUIET
 from . import utils
 from . import legacy
 from . import rdf
-from .rdf import get, add, set_, context
+from .rdf import get, add, set_, context, query
